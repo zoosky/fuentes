6cc0b7c94386011fba7917148441a49cef1cd3a9
Jason Clark <mithereal@gmail.com>
2017-12-08 15:18:10 -0700

    change env call

--- lib/fuentes/config.ex
+++ lib/fuentes/config.ex
@@ -11,7 +11,7 @@ defmodule Fuentes.Config do
     Selects the first repo from the list of configured `:ecto_repos` and establishes
     it as the default repo argument for those function which use a repo.
   """
-  def repo, do: List.first(Application.get_env!(:fuentes, :ecto_repos))
+  def repo, do: List.first(Application.fetch_env!(:fuentes, :ecto_repos))
 
   #def tenancy, do: Application.get_env(:fuentes, :tenancy)
 end
